60d9d16e4bbce00ed1a1b01b0f84b83d27622a5f
GPAPD <94224847+GPAPD@users.noreply.github.com>
2024-09-10 17:11:49 +0530

    Add Coupon Service

---
+++ ComGroups.Services.CouponAPI/Model/Coupon.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ComGroups.Services.CouponAPI.Model
+{
+	public class Coupon
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		[Key]
+		public int CouponId { get; set; }
+		[Required]
+		public string CouponCode { get; set; }
+		[Required]
+		public double DiscountAmount { get; set; }
+		public double MinAmount { get; set; }
+	}
+}

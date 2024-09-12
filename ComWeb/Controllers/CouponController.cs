1b91da16249115a74dc5355f25cfc6c4777e732c
GPAPD <94224847+GPAPD@users.noreply.github.com>
2024-09-13 02:05:44 +0530

    CouponController added couponApi modified

---
+++ ComWeb/Controllers/CouponController.cs
@@ -0,0 +1,31 @@
+﻿using ComWeb.Models;
+using ComWeb.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Newtonsoft.Json;
+
+namespace ComWeb.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+        public CouponController(ICouponService couponService) 
+        {
+            _couponService = couponService;
+        }
+
+        public async Task<IActionResult> CouponIndex()
+        {
+            List<CouponDto>? couponList = new();
+
+            ResponesDto? responesDto = await _couponService.GetAllCouponAsync();
+
+            if (responesDto != null && responesDto.IsSuccess == true) 
+            {
+                couponList = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(responesDto.Result));
+            }
+
+            return View(couponList);
+        }
+    }
+}

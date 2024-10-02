f3a1a1cd2d5ccb1637bf914d38260b85f74f50a5
GPAPD <94224847+GPAPD@users.noreply.github.com>
2024-10-02 22:38:33 +0530

    add product Service to Web

---
+++ ComWeb/Service/IService/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using ComWeb.Models;
+
+namespace ComWeb.Service.IService
+{
+    public interface IProductService
+    {
+        Task<ResponesDto?> GetAllProdoctsAsync();
+        Task<ResponesDto?> GetProdoctByIdAsync(int Id);
+        Task<ResponesDto?> UpdateProductAsync();
+        Task<ResponesDto?> CreateProductAsync();
+        Task<ResponesDto?> DeleteProductAsync(int Id);
+    }
+}
---
+++ ComWeb/Service/ProductService.cs
@@ -0,0 +1,33 @@
+﻿using ComWeb.Models;
+using ComWeb.Service.IService;
+
+namespace ComWeb.Service
+{
+    public class ProductService : IProductService
+    {
+        public Task<ResponesDto?> CreateProductAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponesDto?> DeleteProductAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponesDto?> GetAllProdoctsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponesDto?> GetProdoctByIdAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponesDto?> UpdateProductAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

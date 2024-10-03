01e5c9b8e07419a66fba7eb64ebfb7ccdc6e482b
GPAPD <94224847+GPAPD@users.noreply.github.com>
2024-10-03 23:43:47 +0530

    bug fix and add get all products

---
+++ ComWeb/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using ComWeb.Models;
+using ComWeb.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ComWeb.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public async Task<IActionResult> ProductIndex()
+        {
+            List <ProductDto> productList = new();
+
+            ResponesDto dto = await _productService.GetAllProdoctsAsync();
+            if (dto != null && dto.IsSuccess == true) 
+            {
+                productList = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(dto.Result));
+            
+            }
+            else 
+            {
+            
+            }
+
+
+            return View(productList);
+        }
+    }
+}
